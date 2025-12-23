481708f99569f7024a8dbba2dd78b1e4db47c06c
Yahia Anas <anas.yahya42@gmail.com>
2025-12-23 22:42:05 +0100

    feat: Enhance Not Found page with dynamic background movement and button styling improvements

--- src/app/global/not-found/not-found.component.ts
+++ src/app/global/not-found/not-found.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ButtonComponent } from '../button/button.component';
@@ -14,4 +14,13 @@ import { LucideAngularModule, ChevronLeft } from 'lucide-angular';
 })
 export class NotFoundComponent {
   readonly ChevronLeft = ChevronLeft;
+  bgTransform = 'translate(0px, 0px)';
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    const x = (e.clientX / window.innerWidth - 0.5) * 20; // -10 to 10px
+    const y = (e.clientY / window.innerHeight - 0.5) * 20; // -10 to 10px
+    
+    this.bgTransform = `translate(${x}px, ${y}px)`;
+  }
 }
