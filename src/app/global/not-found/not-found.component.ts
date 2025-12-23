6b02e44e2b45534fa33c3f29b257fcdd00b26f0b
Yahia Anas <anas.yahya42@gmail.com>
2025-12-23 02:23:52 +0100

    feat: Add CTA section with icon and binary code to Not Found page for improved user interaction

--- src/app/global/not-found/not-found.component.ts
+++ src/app/global/not-found/not-found.component.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ButtonComponent } from '../button/button.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { LucideAngularModule, ChevronLeft } from 'lucide-angular';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [CommonModule, RouterModule, ButtonComponent, TranslateModule],
+  imports: [CommonModule, RouterModule, ButtonComponent, TranslateModule, LucideAngularModule],
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+  readonly ChevronLeft = ChevronLeft;
+}
