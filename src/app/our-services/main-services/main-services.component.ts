971176fe0cea02398090fab4a9ad1ca920ac5a85
Yahia Anas <anas.yahya42@gmail.com>
2025-12-24 14:37:12 +0100

    feat: implement main-services component based on figma design

--- src/app/our-services/main-services/main-services.component.ts
+++ src/app/our-services/main-services/main-services.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LucideAngularModule, ChevronLeft } from 'lucide-angular';
 
 @Component({
   selector: 'app-main-services',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, LucideAngularModule],
   templateUrl: './main-services.component.html',
   styleUrls: ['./main-services.component.scss']
 })
-export class MainServicesComponent {}
+export class MainServicesComponent {
+  readonly ChevronLeft = ChevronLeft;
+  imgService = '/01Service.svg';
+
+  services = [
+    'SERVICES_PAGE.MAIN.LIST.WEB_MOBILE',
+    'SERVICES_PAGE.MAIN.LIST.INTERNAL_PLATFORMS',
+    'SERVICES_PAGE.MAIN.LIST.INTEGRATION',
+    'SERVICES_PAGE.MAIN.LIST.ANALYSIS',
+    'SERVICES_PAGE.MAIN.LIST.UI_UX'
+  ];
+
+  constructor(private translate: TranslateService) {}
+}
