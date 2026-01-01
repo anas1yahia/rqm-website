12ea37da0d1e5b93cc2180273abcac6990b1e01b
Yahia Anas <anas.yahya42@gmail.com>
2026-01-01 15:15:52 +0100

    refactor: Simplify text decoding in hero sections and remove unused component

--- src/app/landing-page/home/home.component.ts
+++ src/app/landing-page/home/home.component.ts
@@ -8,7 +8,6 @@ import { MainLayoutComponent } from '../main-layout/main-layout.component';
 import { InfoSectionComponent } from '../info-section/info-section.component';
 import { TransformationProjectComponent } from '../transformation-project/transformation-project.component';
 import { ProcessSectionComponent } from '../process-section/process-section.component';
-import { OurProductsComponent } from '../our-products/our-products.component';
 import { QualityProofComponent } from '../quality-proof/quality-proof.component';
 import { OurPartnersComponent } from '../our-partners/our-partners.component';
 import { FooterComponent } from '../../global/footer/footer.component';
@@ -25,7 +24,6 @@ import { ScrollFadeDirective } from '../../directives/scroll-fade';
     InfoSectionComponent,
     TransformationProjectComponent,
     ProcessSectionComponent,
-    OurProductsComponent,
     QualityProofComponent,
     OurPartnersComponent,
     FooterComponent,
