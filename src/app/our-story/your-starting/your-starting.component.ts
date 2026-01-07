0d8084e587e6515595f8efe3a46a759161bd115e
Yahia Anas <anas.yahya42@gmail.com>
2026-01-07 16:10:21 +0100

    feat(your-starting): enhance layout and styles for tech section with new components

--- src/app/our-story/your-starting/your-starting.component.ts
+++ src/app/our-story/your-starting/your-starting.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { ScrollFadeDirective } from '../../directives/scroll-fade';
+import { ButtonComponent } from '../../global/button/button.component';
 
 @Component({
   selector: 'app-your-starting',
   standalone: true,
-  imports: [CommonModule, TranslateModule, ScrollFadeDirective],
+  imports: [CommonModule, TranslateModule, ScrollFadeDirective, ButtonComponent],
   templateUrl: './your-starting.component.html',
   styleUrls: ['./your-starting.component.scss']
 })
